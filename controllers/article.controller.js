7e755b6ee1ce3d1347a93ff5dda992f78af55b9c
Milad salamian <miladsalami1385@gmail.com>
2025-02-05 12:36:07 -0800

    add update article controller

--- controllers/article.controller.js
+++ controllers/article.controller.js
@@ -128,3 +128,97 @@ exports.delete = async (req, res, next) => {
     next(error);
   }
 };
+
+exports.update = async (req, res, next) => {
+  try {
+    let { id } = req.params;
+
+    if (!id) {
+      return res.status(422).json({ message: "Please send id in parameters" });
+    }
+
+    let article = await Article.findByPk(id, {
+      include: [
+        {
+          model: Tag,
+          through: {
+            attributes: [],
+          },
+        },
+      ],
+    });
+
+    if (!article) {
+      return res.status(404).json({ message: "Article is not found :(" });
+    }
+
+    let { title, content, tags } = req.body; // frontend -> ["frontend"]
+    let slug = slugify(title, { lower: true });
+    const copyOfSlug = slug;
+    const authorId = req.user.id;
+
+    tags = Array.isArray(tags) ? tags : [tags];
+
+    tags = tags.map((tag) =>
+      Tag.findOrCreate({ where: { title: tag.trim() }, raw: true })
+    );
+    tags = await Promise.all(tags);
+
+    let updatedArticle;
+    let i = 1;
+    const coverPath = `${req.file?.filename}`;
+
+    while (!updatedArticle) {
+      try {
+        updatedArticle = await Article.update(
+          {
+            title,
+            content,
+            slug,
+            author_id: authorId,
+            cover: coverPath,
+          },
+          { where: { id } }
+        );
+
+        return res.status(201).json({
+          ...article.dataValues,
+          tags: tags.map((tag) => tag[0].title),
+        });
+      } catch (err) {
+        if (err.original.code === "ER_DUP_ENTRY") {
+          slug = `${copyOfSlug}-${i++}`;
+        } else {
+          throw err;
+        }
+      }
+
+      await article.removeTags(article.tags);
+
+      const tagsToAdd = tags.map((tag) => tag[0]);
+
+      await article.addTags(tagsToAdd);
+
+      article = await Article.findByPk(id, {
+        attributes: {
+          exclude: ["author_id"],
+        },
+        include: [
+          { model: User, attributes: { exclude: ["password"] }, as: "author" },
+          {
+            model: Tag,
+            through: {
+              attributes: [],
+            },
+          },
+        ],
+      });
+
+      return res
+        .status(200)
+        .json({ message: "article updated successfully :)" });
+    }
+  } catch (error) {
+    next(error);
+  }
+};
